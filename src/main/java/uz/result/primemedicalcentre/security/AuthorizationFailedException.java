17452a78567350c4aeb68d8aff34814182e7b4d6
nodirjon2901 <nodirjontojiahmedov2901@gmail.com>
2025-01-07 12:02:33 +0500

    Commit all by Nodir

--- src/main/java/uz/result/primemedicalcentre/security/AuthorizationFailedException.java
+++
@@ -1,8 +0,0 @@
-package uz.result.primemedicalcentre.security;
-
-public class AuthorizationFailedException extends RuntimeException {
-
-    public AuthorizationFailedException(String message) {
-        super(message);
-    }
-}
